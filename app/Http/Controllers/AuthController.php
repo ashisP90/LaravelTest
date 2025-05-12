94e2314ae1472f45213f3bb48855d40ea2b52b22
Rum Prasad <ashis,prasad90@gmail.com>
2025-05-12 21:32:51 +0530

    update the auth controller

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -15,10 +15,8 @@ class AuthController extends Controller
         if (!$user || !Hash::check($request->password, $user->password)) {
             return response()->json(['message' => 'Invalid credentials'], 401);
         }
-
-        // Check if already logged in (only 1 token allowed)
         if ($user->tokens()->exists()) {
-            return response()->json(['message' => 'User already logged in from another device'], 403);
+            return response()->json(['message' => 'User already logged in from another device. Please logout from there and login again.'], 403);
         }
 
         $token = $user->createToken('auth_token')->plainTextToken;
